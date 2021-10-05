1c041b3cd99f3120b3ec27d1c309639117cd59dc
Ruan van der Westhuizen <rvanderwesthuizen2@jhb.dvt.co.za>
2021-10-05 09:11:26 +0200

    Set up basic project and file structure

---
+++ Mr Movie Info/Mr Movie Info/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mr Movie Info
+//
+//  Created by Ruan van der Westhuizen on 2021/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
