8fcd8a808156b329a2098e9a5e1f2d69144dac17
Ruan van der Westhuizen <rvanderwesthuizen2@jhb.dvt.co.za>
2021-10-27 21:28:48 +0200

    Created onboarding slide struct and view

---
+++ Mr Movie Info/Mr Movie Info/Feature-Onboarding/ViewController/OnboardingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  OnboardingViewController.swift
+//  Mr Movie Info
+//
+//  Created by Ruan van der Westhuizen on 2021/10/27.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    var slides: [OnboardingSlide] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
