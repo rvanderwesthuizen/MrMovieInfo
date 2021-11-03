bdee8f923de256d97050c53267a0fd82330b5bc2
Ruan van der Westhuizen <rvanderwesthuizen2@jhb.dvt.co.za>
2021-11-03 06:59:11 +0200

    Created register view

---
+++ Mr Movie Info/Mr Movie Info/Feature-Login/Welcome/ViewController/WelcomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeViewController.swift
+//  Mr Movie Info
+//
+//  Created by Ruan van der Westhuizen on 2021/11/03.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func loginNowTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+        
+    }
+}
